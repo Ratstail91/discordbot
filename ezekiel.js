817c8fb5f34947260512e6c339296af35feb5a35
Kayne Ruse <kayneruse@gmail.com>
2021-06-05 04:48:42 +1000

    Updated ezekiel

--- ezekiel.js
+++ ezekiel.js
@@ -4,7 +4,6 @@ require('dotenv').config({path: './.env'});
 // Node Modules
 let discord = require('discord.js');
 let client = new discord.Client();
-let { CronJob } = require('cron');
 let { parseAndRoll } = require("roll-parser");
 
 // Bot Modules
@@ -34,13 +33,15 @@ client.on('uncaughtException', console.error);
 
 // The ready event is vital, it means that your bot will only start reacting to information from discord _after_ ready is emitted
 client.on('ready', async () => {
+	/* //don't need this right now, might be left-over from ADAM
 	// Generates invite link
 	try {
-		let link = await client.generateInvite(["SEND_MESSAGES", "MANAGE_MESSAGES"]);
+		let link = await client.generateInvite({ permissions: ["SEND_MESSAGES", "MANAGE_MESSAGES"] });
 		console.log("Invite Link: " + link);
 	} catch(e) {
 		console.log(e.stack || e);
 	}
+	*/
 
 	// You can set status to 'online', 'invisible', 'away', or 'dnd' (do not disturb)
 	client.user.setStatus('online');
@@ -52,12 +53,6 @@ client.on('ready', async () => {
 	}
 
 	console.log("Logged in as: " + client.user.username + " - " + client.user.id);
-
-	//wisdom
-	let wisdomJob = new CronJob("0 7 * * *", () => {
-		sendPublicMessage(client, "KRGameStudios", "bot-spam", dialog("wisdom"));
-	});
-	wisdomJob.start();
 });
 
 // Create an event listener for messages
@@ -69,6 +64,11 @@ client.on('message', async message => {
 
 	// Has to be (prefix)command
 	if (message.content.indexOf(process.env.PREFIX) !== 0) {
+		try {
+			passiveResponses(client, message); //watch passively
+		} catch(e) {
+			console.log(e.stack || e);
+		}
 		return;
 	}
 
@@ -89,6 +89,18 @@ client.on('message', async message => {
 
 //Log our bot in
 client.login(process.env.TOKEN);
+
+//handler functions
+function passiveResponses(client, message) {
+	// "This is the best way to define args. Trust me."
+	// - Some tutorial dude on the internet
+	let args = message.content.slice(process.env.PREFIX.length).trim().split(/ +/g);
+	let command = args.shift().toLowerCase();
+
+	//do passive stuff
+
+	return false;
+}
 
 function processBasicCommands(client, message) {
 	// "This is the best way to define args. Trust me."
